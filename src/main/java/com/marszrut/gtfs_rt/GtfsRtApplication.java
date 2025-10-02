67a36229a2fb0a0c57979eb328d93ae94f07e53c
AK <kizeikova@gmail.com>
2025-10-02 16:04:47 +0200

    init

---
+++ src/main/java/com/marszrut/gtfs_rt/GtfsRtApplication.java
@@ -0,0 +1,13 @@
+package com.marszrut.gtfs_rt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtfsRtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtfsRtApplication.class, args);
+	}
+
+}
